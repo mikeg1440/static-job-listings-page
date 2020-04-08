d2aec22fbbe952906554798b14308cea43f7cef0
Michael Gaudreau <gaudreaum01@gmail.com>
2020-04-08 15:13:41 -0400

    Add tags template for Job component

--- src/components/Job.js
+++ src/components/Job.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 const Job = ({}) => (
   <div className='job-listing'>
+    <div className='logo-container'>
+      <img src='images/myhome.svg' />
+    </div>
 
+    <div className='details-container'>
+      <span>
+        <h2>Company Name</h2>
+        <label>NEW!</label>
+      </span>
+      <h1>Position</h1>
+
+      <span>
+        <p>Age - Duration - Location</p>
+      </span>
+    </div>
+
+    <div className='label-container'>
+      <b>Labels</b>
+      <b>Labels</b>
+      <b>Labels</b>
+    </div>
   </div>
 );
 
