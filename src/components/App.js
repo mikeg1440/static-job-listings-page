cdd28121137ea8d837e229a4a08d67303aaf5942
Michael Gaudreau <gaudreaum01@gmail.com>
2020-04-08 15:03:11 -0400

    Add header image to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import headerImg from '../images/bg-header-desktop.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img className='app-header' src={headerImg} alt='header' />
     </div>
   );
 }
