b8c0f03f44b6135b0ddd405c2e1070732fc5913c
Michael Gaudreau <gaudreaum01@gmail.com>
2020-04-08 20:47:03 -0400

    Add import for uniqid in App component and call it for the key of the main div in component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import uniqid from 'uniqid';
 import headerImg from '../images/bg-header-desktop.svg';
 import Job from './Job';
 import './App.css';
@@ -7,8 +8,8 @@ const data = require('../data.json');
 
 function App() {
   return (
-    <div className="App">
-      <img className='app-header' src={headerImg} alt='header' />
+    <div key={uniqid()} className="App">
+      <img className='app-header' src={headerImg} alt='header banner' />
       {console.table(data)}
       <div className='listing-container'>
         {data.map(listing => <Job key={listing.id} listing={listing} />)}
